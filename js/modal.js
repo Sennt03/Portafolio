6c71c57abd9fa497a3f0ece3ad1c79fb1e5280f0
David <davidrsmk144@gmail.com>
2022-12-26 09:53:27 -0500

    Update links to projects

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,24 @@
 const githubLink = 'https://github.com/Sennt03/'
 
 const img1 = './img/senntcloud.jpg'
-const github1 = githubLink + 'Mean-Stack-Nube-Online'
-const link1 = 'https://senntcloud.herokuapp.com'
+const github1 = githubLink + 'Angular-Node-Nube-Online'
+const link1 = 'https://senntcloud.up.railway.app'
 const nombre1 = 'SenntCloud'
-const tecnologias1 = 'Mean Stack'
+const tecnologias1 = 'Angular, NodeJS y MongoDB'
 const descripcion1 = 'Sistema de nube online donde puedes subir tus archivos, subir imagenes, crear carpetas y desde cualquier dispositivo solo iniciando sesion puedes descargarlos'
 
 const img2 = './img/senntshop.jpg'
 const github2 = githubLink + 'NodeJS-Sistema-Tienda-Online'
-const link2 = 'https://senntshop.herokuapp.com'
+const link2 = 'https://senntshop.up.railway.app'
 const nombre2 = 'SenntShop'
 const tecnologias2 = 'NodeJS y MySql'
 const descripcion2 = 'Sistema de tienda online con NodeJS, javascript moderno y subida de imagenes a cloudinary'
 
 const img3 = './img/senntio.jpg'
-const github3 = githubLink + 'MeanStack-Mini-red-social'
+const github3 = githubLink + 'Angular-Node-Mini-red-social'
 const link3 = 'https://sennt-io.herokuapp.com'
 const nombre3 = 'SenntIO'
-const tecnologias3 = 'Mean Stack'
+const tecnologias3 = 'Angular, NodeJS y MongoDB'
 const descripcion3 = 'Mini red social con Angular, NodeJS y MongoDB. Puedes añadir, editar y borrar publicaciones - Configuracion de perfil - Vista de perfil de todos los usuarios y mas'
 
 const img4 = './img/landingpage.jpg'
@@ -30,10 +30,10 @@ const descripcion4 = 'Landing page simple con css flexbox y css grid'
 
 const img5 = './img/senntblog.jpg'
 const github5 = githubLink + 'Angular-Node-SenntMessages'
-const link5 = 'https://sennt03.github.io/Senntmessages'
+const link5 = 'https://senntmessages.up.railway.app/'
 const nombre5 = 'Sennt Messages'
 const tecnologias5 = 'Angular, NodeJS y Socket.io'
-const descripcion5 = 'App de chat en tiempo real con socket.io (Clon de whatsapp). Contiene creacion de chats, configuracion de perfil, buscador de ususarios, envia mensajes e imagenes. La app esta modularizada, con lazy loading y estrategia de precarga de modulos, tambien use formularios reactivos con validaciones asincronas y NGRX para el manejo del estado en la aplicacion. (Mas detalles en el readme del repositorio)'
+const descripcion5 = 'App de chat en tiempo real con socket.io (Clon de whatsapp). Contiene creacion de chats, configuracion de perfil, buscador de ususarios, envia mensajes, audios, ubicacion, archivos e imagenes. La app esta modularizada, incluye lazy loading y estrategia de precarga de modulos, también use formularios reactivos con validaciones asincronas y NGRX para el manejo del estado en la aplicacion. (Mas detalles en el readme del repositorio)'
 
 const btns = document.querySelectorAll('.detalles')
 
